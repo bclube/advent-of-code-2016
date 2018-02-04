4f0af785301cff458d8c6058b7dece3d478e5637
Brian Lubeski <bclube@gmail.com>
2018-02-03 23:33:27 -0500

    Add tests and solution for day 6b problem

--- lib/AOC/day_06.ex
+++ lib/AOC/day_06.ex
@@ -1,12 +1,15 @@
 defmodule AOC.Day06 do
-  def solve_6a(input) do
+  defp solve_day_6(input, select_fn) do
     input
     |> String.split()
     |> Stream.flat_map(&(&1 |> String.codepoints() |> Stream.with_index()))
     |> Enum.reduce(%{}, &Map.update(&2, &1, 1, fn v -> v + 1 end))
     |> Enum.group_by(fn {{_, i}, _} -> i end, fn {{ch, _}, freq} -> {freq, ch} end)
-    |> Stream.map(fn {idx, vs} -> {idx, Enum.max_by(vs, fn {freq, _} -> freq end)} end)
+    |> Stream.map(fn {idx, vs} -> {idx, select_fn.(vs, fn {freq, _} -> freq end)} end)
     |> Enum.sort_by(fn {idx, _} -> idx end)
     |> Enum.map_join(fn {_, {_, ch}} -> ch end)
   end
+
+  def solve_6a(input), do: solve_day_6(input, &Enum.max_by/2)
+  def solve_6b(input), do: solve_day_6(input, &Enum.min_by/2)
 end
--- test/AOC/day_06_test.exs
+++ test/AOC/day_06_test.exs
@@ -3,25 +3,30 @@ defmodule AOC.Day06Test do
 
   alias AOC.Day06
 
+  @input """
+  eedadn
+  drvtee
+  eandsr
+  raavrd
+  atevrs
+  tsrnev
+  sdttsa
+  rasrtv
+  nssdts
+  ntnada
+  svetve
+  tesnvt
+  vntsnd
+  vrdear
+  dvrsen
+  enarar
+  """
+
   test "Day 6a solution" do
-    assert "easter" ==
-             Day06.solve_6a("""
-             eedadn
-             drvtee
-             eandsr
-             raavrd
-             atevrs
-             tsrnev
-             sdttsa
-             rasrtv
-             nssdts
-             ntnada
-             svetve
-             tesnvt
-             vntsnd
-             vrdear
-             dvrsen
-             enarar
-             """)
+    assert "easter" == Day06.solve_6a(@input)
+  end
+
+  test "Day 6b solution" do
+    assert "advent" == Day06.solve_6b(@input)
   end
 end
