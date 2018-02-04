4a4b6b41f952ad1b21af83562c7cbcebb140c409
Brian Lubeski <bclube@gmail.com>
2018-02-04 00:03:56 -0500

    Refactor day 6 solutions for clarity, perf

--- lib/AOC/day_06.ex
+++ lib/AOC/day_06.ex
@@ -2,12 +2,18 @@ defmodule AOC.Day06 do
   defp solve_day_6(input, select_fn) do
     input
     |> String.split()
-    |> Stream.flat_map(&(&1 |> String.codepoints() |> Stream.with_index()))
+    |> Stream.map(&String.codepoints/1)
+    |> Stream.zip()
+    |> Stream.map(&Tuple.to_list/1)
+    |> Stream.map(&get_result_char(&1, select_fn))
+    |> Enum.join()
+  end
+
+  defp get_result_char(chs, select_fn) do
+    chs
     |> Enum.reduce(%{}, &Map.update(&2, &1, 1, fn v -> v + 1 end))
-    |> Enum.group_by(fn {{_, i}, _} -> i end, fn {{ch, _}, freq} -> {freq, ch} end)
-    |> Stream.map(fn {idx, vs} -> {idx, select_fn.(vs, fn {freq, _} -> freq end)} end)
-    |> Enum.sort_by(fn {idx, _} -> idx end)
-    |> Enum.map_join(fn {_, {_, ch}} -> ch end)
+    |> select_fn.(&elem(&1, 1))
+    |> elem(0)
   end
 
   def solve_6a(input), do: solve_day_6(input, &Enum.max_by/2)
