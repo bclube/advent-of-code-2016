ac001e7fe3f0d5bc793fb366f88662118cdbc6a9
Brian Lubeski <bclube@gmail.com>
2018-02-03 22:25:26 -0500

    Factor out day 5 hash function.

--- lib/AOC/day_05.ex
+++ lib/AOC/day_05.ex
@@ -1,15 +1,22 @@
 defmodule AOC.Day05 do
   def solve_5a(input) do
-    Stream.iterate(0, &(&1 + 1))
-    |> Stream.map(fn v -> input <> Integer.to_string(v) end)
-    |> Stream.map(&:erlang.md5/1)
-    |> Stream.flat_map(fn
-      <<0::size(20), c::size(4), _rest::binary>> -> [c]
-      _ -> []
-    end)
+    input
+    |> find_interesting_hashes()
+    |> Stream.map(fn %{sixth_char: v} -> v end)
     |> Stream.take(8)
     |> Stream.chunk_every(2)
     |> Enum.reduce(<<>>, fn [a, b], acc -> acc <> <<a::size(4), b::size(4)>> end)
     |> Base.encode16(case: :lower)
+  end
+
+  defp find_interesting_hashes(input) do
+    Stream.iterate(0, &(&1 + 1))
+    |> Stream.map(fn v -> input <> Integer.to_string(v) end)
+    |> Stream.map(&:erlang.md5/1)
+    |> Stream.flat_map(fn
+      <<0::size(20), sixth_char::size(4), seventh_char::size(4), _::size(4), _rest::binary>> ->
+        [%{sixth_char: sixth_char, seventh_char: seventh_char}]
+      _ -> []
+    end)
   end
 end
