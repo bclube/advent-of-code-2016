41a5081574577f39a16eb65ecaf1b4b3771573e2
Brian Lubeski <bclube@gmail.com>
2018-03-03 23:00:48 -0500

    Use bitwise (instead of bitstring) ops for speed/efficiency.

--- lib/AOC/day_11.ex
+++ lib/AOC/day_11.ex
@@ -12,13 +12,13 @@ defmodule AOC.Day11 do
       thulium: 1 <<< 6
     }
 
-    def empty_floor, do: <<0::16>>
+    def empty_floor, do: 0
 
-    def floor(generators, microchips) do
-      gen = construct(generators)
+    def new_floor(generators, microchips) do
+      gen = construct(generators) <<< 7
       mic = construct(microchips)
 
-      <<gen::8, mic::8>>
+      gen + mic
     end
 
     defp construct(elements) do
@@ -26,36 +26,67 @@ defmodule AOC.Day11 do
       |> Stream.map(&Map.fetch!(@vals, &1))
       |> Enum.reduce(0, &bor/2)
     end
+
+    @microchip_mask 0b1111111
+    @floor_mask 0b11111111111111
+    @floor_2_shift 14
+    @floor_3_shift 28
+    @floor_4_shift 42
+    @loc_shift 56
+
+    def generators(floor), do: floor >>> 7
+    def microchips(floor), do: floor &&& @microchip_mask
+
+    def win_state(state) do
+      floor_4 = floor(state, 1) ||| floor(state, 2) ||| floor(state, 3) ||| floor(state, 4)
+
+      0
+      |> set_loc(4)
+      |> set_floor(4, floor_4)
+    end
+
+    def loc(state), do: state >>> @loc_shift
+    def floor(state, 1), do: state &&& @floor_mask
+    def floor(state, 2), do: state >>> @floor_2_shift &&& @floor_mask
+    def floor(state, 3), do: state >>> @floor_3_shift &&& @floor_mask
+    def floor(state, 4), do: state >>> @floor_4_shift &&& @floor_mask
+
+    @floor_1_mask 0b1111111111111111111111111111111111111111111111111100000000000000
+    @floor_2_mask 0b1111111111111111111111111111111111110000000000000011111111111111
+    @floor_3_mask 0b1111111111111111111111000000000000001111111111111111111111111111
+    @floor_4_mask 0b1111111100000000000000111111111111111111111111111111111111111111
+    @loc_mask 0b0000000011111111111111111111111111111111111111111111111111111111
+
+    def set_loc(state, loc), do: (state &&& @loc_mask) ||| loc <<< @loc_shift
+    def set_floor(state, 1, floor), do: (state &&& @floor_1_mask) ||| floor
+    def set_floor(state, 2, floor), do: (state &&& @floor_2_mask) ||| floor <<< @floor_2_shift
+    def set_floor(state, 3, floor), do: (state &&& @floor_3_mask) ||| floor <<< @floor_3_shift
+    def set_floor(state, 4, floor), do: (state &&& @floor_4_mask) ||| floor <<< @floor_4_shift
   end
 
   defmodule Rules do
-    def win?(<<4::8, x::8, x::8, 0::48>>), do: true
-    def win?(_), do: false
-
     def valid_floor?(floor) do
-      case <<floor::16>> do
-        <<0::8, _microchips::8>> ->
-          true
-
-        <<generators::8, microchips::8>> ->
-          0 == (microchips ^^^ generators &&& microchips)
-      end
+      generators = State.generators(floor)
+      microchips = State.microchips(floor)
+      0 == generators || 0 == (generators ^^^ microchips &&& microchips)
     end
   end
 
   defp start_state do
-    loc = 1
-    floor4 = State.empty_floor()
-    floor3 = State.empty_floor()
-
-    floor2 =
-      State.floor([], [
+    0
+    |> State.set_loc(1)
+    |> State.set_floor(4, State.empty_floor())
+    |> State.set_floor(3, State.empty_floor())
+    |> State.set_floor(
+      2,
+      State.new_floor([], [
         :polonium,
         :promethium
       ])
-
-    floor1 =
-      State.floor(
+    )
+    |> State.set_floor(
+      1,
+      State.new_floor(
         [
           :cobalt,
           :polonium,
@@ -69,23 +100,24 @@ defmodule AOC.Day11 do
           :thulium
         ]
       )
-
-    <<loc::8>> <> floor4 <> floor3 <> floor2 <> floor1
+    )
   end
 
   defp start_state_b do
-    loc = 1
-    floor4 = State.empty_floor()
-    floor3 = State.empty_floor()
-
-    floor2 =
-      State.floor([], [
+    0
+    |> State.set_loc(1)
+    |> State.set_floor(4, State.empty_floor())
+    |> State.set_floor(3, State.empty_floor())
+    |> State.set_floor(
+      2,
+      State.new_floor([], [
         :polonium,
         :promethium
       ])
-
-    floor1 =
-      State.floor(
+    )
+    |> State.set_floor(
+      1,
+      State.new_floor(
         [
           :cobalt,
           :dilithium,
@@ -103,8 +135,7 @@ defmodule AOC.Day11 do
           :thulium
         ]
       )
-
-    <<loc::8>> <> floor4 <> floor3 <> floor2 <> floor1
+    )
   end
 
   def next(1), do: [2]
@@ -112,39 +143,34 @@ defmodule AOC.Day11 do
   def next(3), do: [4, 2]
   def next(4), do: [3]
 
-  def combinations(<<generators::8, microchips::8>>) do
-    bits = Enum.map(0..7, &bsl(1, &1))
-    both = generators &&& microchips
+  @bits Enum.map(0..7, &bsl(1, &1))
+
+  def combinations(floor) do
+    generators = State.generators(floor)
+    microchips = State.microchips(floor)
 
     gen_bits =
-      bits
+      @bits
       |> Stream.map(&band(generators, &1))
       |> Enum.reject(&(&1 == 0))
 
     micro_bits =
-      bits
+      @bits
       |> Stream.map(&band(microchips, &1))
       |> Enum.reject(&(&1 == 0))
 
     gen_micro =
-      bits
-      |> Stream.map(&band(both, &1))
+      @bits
+      |> Stream.map(&band(generators &&& microchips, &1))
       |> Stream.reject(&(&1 == 0))
-      |> Stream.map(fn v ->
-        <<vv::16>> = <<v::8, v::8>>
-        vv
-      end)
+      |> Stream.map(fn v -> (v <<< 7) + v end)
 
     two_gen =
       gen_bits
       |> Stream.iterate(&Enum.drop(&1, 1))
       |> Stream.take_while(fn l -> not Enum.empty?(l) end)
       |> Stream.flat_map(fn [v | vs] ->
-        Stream.map(vs, fn v2 ->
-          combined = v ||| v2
-          <<vv::16>> = <<combined::8, 0::8>>
-          vv
-        end)
+        Stream.map(vs, fn v2 -> (v ||| v2) <<< 7 end)
       end)
 
     two_micro =
@@ -152,60 +178,30 @@ defmodule AOC.Day11 do
       |> Stream.iterate(&Enum.drop(&1, 1))
       |> Stream.take_while(fn l -> not Enum.empty?(l) end)
       |> Stream.flat_map(fn [v | vs] ->
-        Stream.map(vs, fn v2 ->
-          combined = v ||| v2
-          <<vv::16>> = <<0::8, combined::8>>
-          vv
-        end)
+        Stream.map(vs, fn v2 -> v ||| v2 end)
       end)
 
     one_gen =
       gen_bits
-      |> Stream.map(fn v ->
-        <<vv::16>> = <<v::8, 0::8>>
-        vv
-      end)
-
-    one_micro =
-      micro_bits
-      |> Stream.map(fn v ->
-        <<vv::16>> = <<0::8, v::8>>
-        vv
-      end)
+      |> Stream.map(fn v -> v <<< 7 end)
 
     Enum.concat([
       gen_micro,
       two_gen,
       two_micro,
       one_gen,
-      one_micro
+      micro_bits
     ])
   end
 
-  def get_floor(1, floor1, _f2, _f3, _f4), do: floor1
-  def get_floor(2, _f1, floor2, _f3, _f4), do: floor2
-  def get_floor(3, _f1, _f2, floor3, _f4), do: floor3
-  def get_floor(4, _f1, _f2, _f3, floor4), do: floor4
-
-  def update_floor(<<_loc::8, floor4::16, floor3::16, floor2::16, _floor1::16>>, 1, floor),
-    do: <<1::8, floor4::16, floor3::16, floor2::16, floor::16>>
-
-  def update_floor(<<_loc::8, floor4::16, floor3::16, _floor2::16, floor1::16>>, 2, floor),
-    do: <<2::8, floor4::16, floor3::16, floor::16, floor1::16>>
-
-  def update_floor(<<_loc::8, floor4::16, _floor3::16, floor2::16, floor1::16>>, 3, floor),
-    do: <<3::8, floor4::16, floor::16, floor2::16, floor1::16>>
-
-  def update_floor(<<_loc::8, _floor4::16, floor3::16, floor2::16, floor1::16>>, 4, floor),
-    do: <<4::8, floor::16, floor3::16, floor2::16, floor1::16>>
-
-  def next_moves(<<loc::8, floor4::16, floor3::16, floor2::16, floor1::16>> = state) do
-    floor = get_floor(loc, floor1, floor2, floor3, floor4)
-    combos = combinations(<<floor::16>>)
+  def next_moves(state) do
+    loc = State.loc(state)
+    floor = State.floor(state, loc)
+    combos = combinations(floor)
     next_loc = next(loc)
 
     for n_loc <- next_loc,
-        n_floor = get_floor(n_loc, floor1, floor2, floor3, floor4),
+        n_floor = State.floor(state, n_loc),
         cargo <- combos,
         from_floor = floor ^^^ cargo,
         Rules.valid_floor?(from_floor),
@@ -213,24 +209,26 @@ defmodule AOC.Day11 do
         Rules.valid_floor?(to_floor),
         do:
           state
-          |> update_floor(loc, from_floor)
-          |> update_floor(n_loc, to_floor)
+          |> State.set_loc(n_loc)
+          |> State.set_floor(loc, from_floor)
+          |> State.set_floor(n_loc, to_floor)
   end
 
   defp solve_day_ll(state) do
-    q = :queue.from_list([{state, []}])
+    win_state = State.win_state(state)
+    q = :queue.from_list([{state, 0}])
     seen = MapSet.new()
 
     Stream.unfold({q, seen}, fn {nxt, seen} ->
       case :queue.out(nxt) do
-        {{:value, {st, pth} = v}, new_q} ->
+        {{:value, {st, path_len} = v}, new_q} ->
           next = next_moves(st) |> Enum.reject(&MapSet.member?(seen, &1))
 
           new_queue =
             Enum.reduce(next, new_q, fn nxt, q ->
-              n = {nxt, [st | pth]}
+              n = {nxt, path_len + 1}
 
-              if not Rules.win?(nxt),
+              if nxt != win_state,
                 do: :queue.in(n, q),
                 else: :queue.in_r(n, q)
             end)
@@ -243,10 +241,9 @@ defmodule AOC.Day11 do
           nil
       end
     end)
-    |> Enum.find_value(fn {state, path} ->
-      if Rules.win?(state), do: path
+    |> Enum.find_value(fn {state, path_len} ->
+      if state == win_state, do: path_len
     end)
-    |> Enum.count()
   end
 
   def solve_11a, do: start_state() |> solve_day_ll()
